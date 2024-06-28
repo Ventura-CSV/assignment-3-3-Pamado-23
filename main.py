3d4a17359c5ee3b5502cbefe2534f6c2f20606c3
Pamado-23 <pamado1@my.vcccd.edu>
2024-06-27 23:09:34 -0700

    Assignment 3-3

--- main.py
+++ main.py
@@ -5,7 +5,22 @@ def main():
     Code Your Program here
     ########################################
     """
+
     result = True
+
+    if not (email[0].isalpha()):
+        result = False
+    elif not (5 < len(email) < 30):
+        result = False
+    elif email.find('@') == -1:
+        result = False
+    else:
+        index = email.index('@')
+        if email.find('.', index) == -1:
+            result = False
+
+    print (f'{result}')
+    
 
     ########################################
     # Do not delete the return statement
